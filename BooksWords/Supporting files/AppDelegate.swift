e13eb940ba2830094094cedb350ffea861471cca
Emanuele Beffa <emanuele.beffa@me.com>
2019-04-20 17:13:07 +0200

    First commit

---
+++ BooksWords/Supporting files/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  BooksWords
+//
+//  Created by Emanuele Beffa on 20/04/2019.
+//  Copyright © 2019 Emanuele Beffa. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ BooksWordsTests/BooksWordsTests.swift
@@ -0,0 +1,21 @@
+//
+//  BooksWordsTests.swift
+//  BooksWordsTests
+//
+//  Created by Emanuele Beffa on 20/04/2019.
+//  Copyright © 2019 Emanuele Beffa. All rights reserved.
+//
+
+import XCTest
+@testable import BooksWords
+
+class BooksWordsTests: XCTestCase {
+    
+    override func setUp() {
+        
+    }
+    
+    override func tearDown() {
+        
+    }
+}
